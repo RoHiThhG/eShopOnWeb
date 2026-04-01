3d2a10107acfbcdf4b7e72867831a7e80f00a73c
RoHiThhG <rohith_g@epam.com>
2026-04-01 12:30:07 +0530

    program.cs

--- src/PublicApi/Program.cs
+++ src/PublicApi/Program.cs
@@ -17,6 +17,10 @@ using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Test startup error - to be removed after testing
+throw new Exception("Cannot move further");
+
+
 // Add service defaults & Aspire components.
 builder.AddAspireServiceDefaults();
 
